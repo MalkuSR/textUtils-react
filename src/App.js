02df1b5730c75f5e13814de72432dfe9f8bb605b
MalkuSR <m.s.malik3105@gmail.com>
2024-08-02 10:24:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+// import About from './components/about';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import Alert from './components/Alert';
 
 function App() {
+  const [mode, setMode] = useState('light')
+  const [modetext, setModeText] = useState('Switch Dark')
+  const [navstyle, setNavStyle] = useState({
+    color: "black"
+  })
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
+
+  const togglemode = () => {
+    if (mode === 'light') {
+      setMode('dark')
+      setModeText('Switch Light')
+      setNavStyle({
+        color: 'white'
+      })
+      document.body.style.backgroundColor = 'rgb(12 25 36)'
+      showAlert(" Dark mode has been enabled", "Success");
+      setTimeout(() => {
+        setAlert(null)
+      }, 2000)
+    }
+    else {
+      setMode('light')
+      setModeText('Switch Dark')
+      setNavStyle({
+        color: 'black'
+      })
+      document.body.style.backgroundColor = 'white'
+      showAlert(" Light mode has been enabled", "Success");
+      setTimeout(() => {
+        setAlert(null)
+      }, 2000)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {/* <Router> */}
+      <Navbar title="TextUtils" aboutText="About Us" mode={mode} togglemode={togglemode} modetext={modetext} navstyle={navstyle} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+      {/* <Routes> */}
+          {/* <Route exact path="/" element={ */}
+            <Textform Heading="Enter the text to analyze" mode={mode} />
+            {/* } /> */}
+          {/* <Route exact path="/about" element={<About />} /> */}
+        {/* </Routes> */}
+      </div>
+    {/* </Router> */}
+    </>
+  )
 }
 
 export default App;
