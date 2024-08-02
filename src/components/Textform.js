1ff0efd0270b01c8e0b478ad9b5b487e7620fd68
MalkuSR <m.s.malik3105@gmail.com>
2024-08-02 12:51:48 +0500

    Update Textform.js

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-export default function Textform(props) {
+export default function Textform({mode, Heading} ) {
     const handleUpClick = () => {
         let newText = text.toUpperCase()
         setText(newText)
@@ -51,28 +51,28 @@ export default function Textform(props) {
     const [text, setText] = useState("");
     return (
         <>
-            <div className="container mt-5" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
-                <h1>{props.Heading} </h1>
+            <div className="container mt-5" style={{ color: mode === 'dark' ? 'white' : 'black' }}>
+                <h1>{Heading} </h1>
                 <div className="mb-3 ">
-                    <textarea value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'grey' : '#e1e1e1', color: props.mode === 'dark' ? 'white' : 'black' }} className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
+                    <textarea value={text} onChange={handleOnChange} style={{ backgroundColor: mode === 'dark' ? 'grey' : '#e1e1e1', color: mode === 'dark' ? 'white' : 'black' }} className="form-control" id="exampleFormControlTextarea1" rows="8"></textarea>
                 </div>
-                <button onClick={handleUpClick} className="btn btn-primary mx-1">Convert to upercase</button>
+                <button onClick={handleUpClick} className="btn btn-primary mx-1 my-1">Convert to upercase</button>
                 {notification2 && <div className="notification">Converted to uper case!</div>}
-                <button onClick={convertToLower} className="btn btn-primary mx-1">Convert to lowercase</button>
+                <button onClick={convertToLower} className="btn btn-primary mx-1 my-1">Convert to lowercase</button>
                 {notification3 && <div className="notification">Converted to lower case!</div>}
-                <button onClick={handleClearClick} className="btn btn-primary mx-1">clear</button>
+                <button onClick={handleClearClick} className="btn btn-primary mx-1 my-1">clear</button>
                 {clear && <div className="clear">Cleared</div>}
                 <button onClick={handleCopyClick} className="btn btn-primary mx-1">Copy</button>
                 {notification4 && <div className="notification">Copied to clipboard!</div>}
-                <button onClick={handleEmailCopyClick} className="btn btn-primary mx-1">Copy Email</button>
+                <button onClick={handleEmailCopyClick} className="btn btn-primary mx-1 my-1">Copy Email</button>
                 {email && <div className="email">Email Copied to clipboard!</div>}
-                <button onClick={handleRemoveExtrasapcesClick} className="btn btn-primary mx-1">Remove Extra Spaces</button>
+                <button onClick={handleRemoveExtrasapcesClick} className="btn btn-primary mx-1 my-1">Remove Extra Spaces</button>
                 {notification && <div className="notification">Extra spaces removed!</div>}
             </div>
-            <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+            <div className="container" style={{ color: mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Your text summary</h2>
-                <p className='fw-bold'><span className="text-success">{text.length === 0 ? '0' : text.split(" ").length}</span> words and <span className="text-success">{text.length === 0 ? '0' : text.length}</span> characters </p>
-                <p className='fw-bold'><span className="text-success">{0.008 * text.split(" ").length}</span> minutes read </p>
+                <p className='fw-bold'><span className="text-success">{text.split(" ").filter((element)=>{return element.length !==0}).length}</span> words and <span className="text-success">{text.length === 0 ? '0' : text.length}</span> characters </p>
+                <p className='fw-bold'><span className="text-success">{0.008 *text.split(" ").filter((element)=>{return element.length !==0}).length}</span> minutes read </p>
                 <h2>Preview</h2>
                 <p>{text.length > 0 ? text : "Enter your text to preview here"}</p>
             </div>
